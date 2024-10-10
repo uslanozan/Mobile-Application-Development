814faadd40c17f46eb503e4a042788a38f19afdd
U210709002 <uslanozan@gmail.com>
2024-10-10 12:31:52 +0300

    Part1 of Week3 is commited

---
+++ app/src/main/java/msku/ceng/madlab/week3/MainActivity.java
@@ -0,0 +1,51 @@
+package msku.ceng.madlab.week3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    List<Post> postList = new ArrayList<>();
+    Button btnPost;
+    static final int POST_REQUEST = 48;
+    // 1 olmasının hiçbir önemi yok 3 5 de yapabilirsin
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        listView = findViewById(R.id.listView);
+        PostAdapter postAdapter = new PostAdapter(this, postList);
+
+        listView.setAdapter(postAdapter);
+        btnPost = findViewById(R.id.btnPost);
+        btnPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PostActivity.class);
+                startActivityForResult(intent, POST_REQUEST);
+            }
+        });
+
+    }
+}
