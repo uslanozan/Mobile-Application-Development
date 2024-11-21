55372cd21081d139a935ff1ad6957556ffd43550
U210709002 <uslanozan@gmail.com>
2024-11-21 12:08:13 +0300

    First part of Week7

---
+++ Week7/app/src/main/java/msku/ceng/madlab/week7/BoardView.java
@@ -0,0 +1,20 @@
+package msku.ceng.madlab.week7;
+
+public interface BoardView {
+
+    char PLAYER_1_SYMBOL = 'X';
+    char PLAYER_2_SYMBOL = 'O';
+
+    byte DRAW=0;
+    byte PLAYER_1_WINNER=1;
+    byte PLAYER_2_WINNER=2;
+
+    void newGame();
+
+    void putSymbol(char symbol, byte row, byte col);
+
+    void gameEnded(byte winner);
+
+    void invalidPLay(byte row, byte col);
+
+}
